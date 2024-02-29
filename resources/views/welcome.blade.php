8dc5344cac6673650107bf5f4ec6fecd30b86bf6
Jean carlos <202100363d@utea.edu.pe>
2024-02-29 09:07:02 -0500

    1

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>hola MUNDO</h1>
+    <img src="https://drive.google.com/file/d/1csmMKJw1K68zTLHSXRuqSVxOXk597CEJ/view?usp=sharing" alt="">
+</body>
+</html>
